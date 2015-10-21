32eaebf582859096244dd726e62757560e2bf9aa
slidenerd <slidenerd@gmail.com>
2015-10-21 22:33:27 +0530

    Expand collapse items showing a read more button when there are more than 200 characters and set values dynamically

--- app/src/main/java/slidenerd/vivz/fpam/ui/ActivityPost.java
+++ app/src/main/java/slidenerd/vivz/fpam/ui/ActivityPost.java
@@ -2,13 +2,38 @@ package slidenerd.vivz.fpam.ui;
 
 import android.support.v7.app.AppCompatActivity;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import slidenerd.vivz.fpam.R;
+import slidenerd.vivz.fpam.widget.PostView;
 
 /**
  * Created by vivz on 20/10/15.
  */
 @EActivity(R.layout.sample_post_view)
 public class ActivityPost extends AppCompatActivity {
+    @ViewById(R.id.post_view_1)
+    PostView mPost1;
+    @ViewById(R.id.post_view_2)
+    PostView mPost2;
+
+    @ViewById(R.id.post_view_3)
+    PostView mPost3;
+
+    @AfterViews
+    void onCreateView() {
+        mPost1.setUserName("Vijai Chander");
+        mPost1.setUpdatedTime("5 mins ago");
+        mPost1.setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
+
+        mPost2.setUserName("Ankush Deshpande");
+        mPost2.setUpdatedTime("20 mins ago");
+        mPost2.setMessage("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
+
+        mPost3.setUserName("Sex Poster");
+        mPost3.setUpdatedTime("2 hours ago");
+        mPost3.setMessage("What the hell is going on guys?");
+    }
 }
--- app/src/main/java/slidenerd/vivz/fpam/widget/PostView.java
+++ app/src/main/java/slidenerd/vivz/fpam/widget/PostView.java
@@ -8,8 +8,8 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
-import android.text.DynamicLayout;
 import android.text.Layout;
+import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.TypedValue;
@@ -43,9 +43,9 @@ public class PostView extends View {
     private float mTextWidth;
     private float mTextHeight;
 
-    private String mStringUserName = "Vivek Ramesh";
-    private String mStringUpdatedTime = "2 mins ago";
-    private String mStringMessage = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    private String mStringUserName = null;
+    private String mStringUpdatedTime = null;
+    private String mStringMessage = null;
     private String mStringReadMore = "Read More";
     private Context mContext;
 
@@ -62,7 +62,7 @@ public class PostView extends View {
     private Rect mRectMessage;
     private Rect mRectReadMore;
 
-    private DynamicLayout mLayout;
+    private StaticLayout mLayout;
 
     private float mTextSizeUserName;
     private float mTextSizeUdpatedTime;
@@ -71,7 +71,8 @@ public class PostView extends View {
 
     private RectF mBoundsReadMore;
 
-    private boolean mCollapsed = false;
+    private boolean mCollapsed = true;
+    private boolean mShowReadMore = false;
 
 
     public PostView(Context context) {
@@ -122,7 +123,6 @@ public class PostView extends View {
         mPaintContent = new TextPaint();
         mPaintContent.setFlags(Paint.ANTI_ALIAS_FLAG);
         mPaintContent.setTextAlign(Paint.Align.LEFT);
-        mPaintContent.setTextSize(mTextSizeMessage);
 
         mPaddingLeft = getPaddingLeft();
         mPaddingRight = getPaddingRight();
@@ -138,7 +138,6 @@ public class PostView extends View {
         mRectReadMore = new Rect();
         mBoundsReadMore = new RectF();
 
-        mLayout = new DynamicLayout(mCollapsed ? mStringMessage.substring(0, 100) : mStringMessage, mPaintContent, mPostWidth - mPaddingLeft - mPaddingRight, Layout.Alignment.ALIGN_NORMAL, 1.0F, 1.0F, true);
     }
 
     private int calculateHeight() {
@@ -146,21 +145,29 @@ public class PostView extends View {
         height += mPaddingTop;
         height += mPaddingBottom;
 
-        mPaintContent.setTextSize(mTextSizeUserName);
-        mPaintContent.getTextBounds(mStringUserName, 0, mStringUserName.length(), mRectUserName);
-        height += mRectUserName.height();
+        if (mStringUserName != null && !mStringUserName.equals("")) {
+            mPaintContent.setTextSize(mTextSizeUserName);
+            mPaintContent.getTextBounds(mStringUserName, 0, mStringUserName.length(), mRectUserName);
+            height += mRectUserName.height();
+        }
 
-        mPaintContent.setTextSize(mTextSizeUdpatedTime);
-        mPaintContent.getTextBounds(mStringUpdatedTime, 0, mStringUpdatedTime.length(), mRectUpdatedTime);
-        height += mRectUpdatedTime.height();
+        if (mStringUpdatedTime != null && !mStringUpdatedTime.equals("")) {
+            mPaintContent.setTextSize(mTextSizeUdpatedTime);
+            mPaintContent.getTextBounds(mStringUpdatedTime, 0, mStringUpdatedTime.length(), mRectUpdatedTime);
+            height += mRectUpdatedTime.height();
+        }
 
-        mPaintContent.setTextSize(mTextSizeMessage);
-        mPaintContent.getTextBounds(mStringMessage, 0, mStringMessage.length(), mRectMessage);
-        height += mLayout.getHeight();
+        if (mStringMessage != null && !mStringMessage.equals("")) {
+            mPaintContent.setTextSize(mTextSizeMessage);
+            mPaintContent.getTextBounds(mStringMessage, 0, mStringMessage.length(), mRectMessage);
+            height += mLayout.getHeight();
+        }
 
-        mPaintContent.setTextSize(mTextSizeReadMore);
-        mPaintContent.getTextBounds(mStringReadMore, 0, mStringReadMore.length(), mRectReadMore);
-        height += mRectReadMore.height();
+        if (mShowReadMore) {
+            mPaintContent.setTextSize(mTextSizeReadMore);
+            mPaintContent.getTextBounds(mStringReadMore, 0, mStringReadMore.length(), mRectReadMore);
+            height += mRectReadMore.height();
+        }
 
         return height;
     }
@@ -191,35 +198,43 @@ public class PostView extends View {
         float x = mPaddingLeft;
         float y = mPaddingTop;
 
-        mPaintContent.setTextSize(mTextSizeUserName);
-        mPaintContent.setColor(DisplayUtils.getResolvedColor(mContext, R.color.colorPrimary));
-        mPaintContent.getTextBounds(mStringUserName, 0, mStringUserName.length(), mRectUserName);
-        y += mRectUserName.height();
-        canvas.drawText(mStringUserName, x, y, mPaintContent);
+        if (mStringUserName != null && !mStringUserName.equals("")) {
+            mPaintContent.setTextSize(mTextSizeUserName);
+            mPaintContent.setColor(DisplayUtils.getResolvedColor(mContext, R.color.colorPrimary));
+            mPaintContent.getTextBounds(mStringUserName, 0, mStringUserName.length(), mRectUserName);
+            y += mRectUserName.height();
+            canvas.drawText(mStringUserName, x, y, mPaintContent);
+        }
 
-        mPaintContent.setTextSize(mTextSizeUdpatedTime);
-        mPaintContent.setColor(DisplayUtils.getResolvedColor(mContext, R.color.colorTextSecondary));
-        mPaintContent.getTextBounds(mStringUpdatedTime, 0, mStringUpdatedTime.length(), mRectUpdatedTime);
-        y += mRectUpdatedTime.height();
-        canvas.drawText(mStringUpdatedTime, x, y, mPaintContent);
+        if (mStringUpdatedTime != null && !mStringUpdatedTime.equals("")) {
+            mPaintContent.setTextSize(mTextSizeUdpatedTime);
+            mPaintContent.setColor(DisplayUtils.getResolvedColor(mContext, R.color.colorTextSecondary));
+            mPaintContent.getTextBounds(mStringUpdatedTime, 0, mStringUpdatedTime.length(), mRectUpdatedTime);
+            y += mRectUpdatedTime.height();
+            canvas.drawText(mStringUpdatedTime, x, y, mPaintContent);
+        }
 
-        mPaintContent.setTextSize(mTextSizeMessage);
-        mPaintContent.setColor(DisplayUtils.getResolvedColor(mContext, R.color.colorTextPrimary));
-        mPaintContent.getTextBounds(mStringMessage, 0, mStringMessage.length(), mRectMessage);
-        canvas.save();
-        canvas.translate(x, y);
+        if (mStringMessage != null && !mStringMessage.equals("")) {
+            mPaintContent.setTextSize(mTextSizeMessage);
+            mPaintContent.setColor(DisplayUtils.getResolvedColor(mContext, R.color.colorTextPrimary));
+            mPaintContent.getTextBounds(mStringMessage, 0, mStringMessage.length(), mRectMessage);
+            canvas.save();
+            canvas.translate(x, y);
+            //draws static layout on canvas
+            mLayout.draw(canvas);
+            canvas.restore();
+            y += mLayout.getHeight();
+        }
 
-        //draws static layout on canvas
-        mLayout.draw(canvas);
-        canvas.restore();
+        if (mShowReadMore) {
+            mPaintContent.setTextSize(mTextSizeReadMore);
+            mPaintContent.setColor(DisplayUtils.getResolvedColor(mContext, R.color.colorPrimary));
+            mPaintContent.getTextBounds(mStringReadMore, 0, mStringReadMore.length(), mRectReadMore);
+            y += mRectReadMore.height();
+            canvas.drawText(mStringReadMore, mPostWidth - mPaddingRight - mRectReadMore.width(), y, mPaintContent);
+            mBoundsReadMore.set(mPostWidth - mPaddingRight - mRectReadMore.width(), y - mRectReadMore.height(), mPostWidth - mPaddingRight, y);
+        }
 
-        y += mLayout.getHeight();
-        mPaintContent.setTextSize(mTextSizeReadMore);
-        mPaintContent.setColor(DisplayUtils.getResolvedColor(mContext, R.color.colorPrimary));
-        mPaintContent.getTextBounds(mStringReadMore, 0, mStringReadMore.length(), mRectReadMore);
-        y += mRectReadMore.height();
-        canvas.drawText(mStringReadMore, mPostWidth - mPaddingRight - mRectReadMore.width(), y, mPaintContent);
-        mBoundsReadMore.set(mPostWidth - mPaddingRight - mRectReadMore.width(), y - mRectReadMore.height(), mPostWidth - mPaddingRight, y);
     }
 
     public String getUserName() {
@@ -228,6 +243,7 @@ public class PostView extends View {
 
     public void setUserName(String userName) {
         mStringUserName = userName;
+        invalidate();
     }
 
     public String getUpdatedTime() {
@@ -236,6 +252,13 @@ public class PostView extends View {
 
     public void setUpdatedTime(String updatedTime) {
         mStringUpdatedTime = updatedTime;
+        invalidate();
+    }
+
+    public void setMessage(String message) {
+        mStringMessage = message;
+        initDynamicLayout();
+        requestLayout();
     }
 
     @Override
@@ -244,7 +267,7 @@ public class PostView extends View {
         float touchY = event.getY();
         switch (event.getAction()) {
             case MotionEvent.ACTION_UP:
-                if (mBoundsReadMore.contains(touchX, touchY)) {
+                if (mShowReadMore && mBoundsReadMore.contains(touchX, touchY)) {
                     L.m("yes " + touchX + " " + touchY + " " + mBoundsReadMore);
                     expandCollapseMessage();
                 }
@@ -256,8 +279,16 @@ public class PostView extends View {
 
     private void expandCollapseMessage() {
         mCollapsed = !mCollapsed;
-        mPaintContent.setTextSize(mTextSizeMessage);
-        mLayout = new DynamicLayout(mCollapsed ? mStringMessage.substring(0, 100) : mStringMessage, mPaintContent, mPostWidth - mPaddingLeft - mPaddingRight, Layout.Alignment.ALIGN_NORMAL, 1.0F, 1.0F, true);
+        initDynamicLayout();
         requestLayout();
+    }
+
+    private void initDynamicLayout() {
+        mPaintContent.setTextSize(mTextSizeMessage);
+        if (mStringMessage != null && !mStringMessage.equals("")) {
+            mShowReadMore = mStringMessage.length() > 200;
+            String text = mStringMessage.length() < 200 ? mStringMessage : mStringMessage.substring(0, 200);
+            mLayout = new StaticLayout(mCollapsed ? text : mStringMessage, mPaintContent, mPostWidth - mPaddingLeft - mPaddingRight, Layout.Alignment.ALIGN_NORMAL, 1.0F, 1.0F, true);
+        }
     }
 }
